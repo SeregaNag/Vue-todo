3a5c8a1b4a0b1af5c5fb4a64055d05ed0c86303b
Бебех Сергей <siarhei.bebekh@belcommunmash.by>
2025-05-20 14:59:58 +0300

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import checker from 'vite-plugin-checker'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    checker({ typescript: true })
+  ],
+  server: {
+    port: 3000,
+    open: true, // автоматически открывать браузер
+    cors: true,
+    proxy: {
+      '/api': {
+        target: 'https://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
+  build: {
+    outDir: 'dist',
+    minify: 'terser',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor': ['vue']
+        }
+      }
+    }
+  }
+})
